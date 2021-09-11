4bd001c4ae5a358f7e4aa6d67a5d22c95524bfd8
Lenivaya <xocada@gmail.com>
2021-09-11 13:38:20 +0300

    rewrite AppController endpoint to make gui work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,37 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Feature } from '@data-story-org/core'
+import {
+  Feature,
+  BootPayload,
+  Server,
+  SerializedDiagram,
+} from '@data-story-org/core';
+import { stringify } from 'flatted';
+
+export class RunDiagramDTO {
+  model: SerializedDiagram;
+}
 
 @Controller()
 export class AppController {
+  storyServer: Server = new Server();
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-	const f = new Feature('Greetings!')
-    return f.original
+    const f = new Feature('Greetings!');
+    return f.original as string;
   }
 
   @Post('boot')
-  postBoot() {
-    return {
-      stories: [],
-      availableNodes: [],
-    };
+  async postBoot(): Promise<BootPayload> {
+    return await this.storyServer.boot();
+  }
+
+  @Post('run')
+  async postRun(@Body() runDiagramDTO: RunDiagramDTO): Promise<string> {
+    const runResult = await this.storyServer.run(runDiagramDTO.model);
+    return stringify(runResult);
   }
 }
